3b4b0b260939878fd988c22e23ccd0e5d0fdbd13
ExRoc <guanzhanpeng.ExRoc@gmail.com>
2023-09-19 01:03:09 +0800

    add Luogu U161288

---
+++ math/number-theory/prime/Luogu-U161288.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+inline long long multLL(long long a, long long b, long long mod) {
+    unsigned long long c = (unsigned long long) a * b -
+                           (unsigned long long)((long double) a / mod * b + 0.5) * mod;
+    if (c < mod) {
+        return c;
+    }
+    return c + mod;
+}
+
+long long fastPowLL(long long res, long long n, long long mod) {
+	long long ans;
+	for (ans = 1; n != 0; n >>= 1) {
+		if ((n & 1) == 1) {
+			ans = multLL(ans, res, mod);
+		}
+		res = multLL(res, res, mod);
+	}
+	return ans % mod;
+}
+
+bool isPrime(long long n) {
+    if (n < 3 || n % 2 == 0) {
+        return n == 2;
+    }
+    int t = __builtin_ctzll(n - 1);
+    long long u = (n - 1) >> t;
+    static vector<long long> baseInt = {2, 7, 61};
+    static vector<long long> baseLL = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+    vector<long long> *base;
+    if (n <= INT_MAX) {
+        base = &baseInt;
+    } else {
+        base = &baseLL;
+    }
+    for (long long a: *base) {
+        long long v = fastPowLL(a, u, n);
+        if (v == 1) {
+            continue;
+        }
+        int s;
+        for (s = 0; s < t; ++s) {
+            if (v == n - 1) {
+                break;
+            }
+            v = multLL(v, v, n);
+        }
+        if (s == t) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    #ifdef ExRoc
+    freopen("test.txt", "r", stdin);
+    #endif // ExRoc
+    ios::sync_with_stdio(false);
+
+    int T;
+    long long a;
+    cin >> T;
+    while (T--) {
+        cin >> a;
+        cout << (isPrime(a)? "YES": "NO") << endl;
+    }
+
+    return 0;
+}
